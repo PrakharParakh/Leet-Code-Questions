b10dc08bd16537c387c75cada8ea6e49b427b4bb
Prakhar Parakh <93584013+PrakharParakh@users.noreply.github.com>
2023-01-20 12:25:08 +0530

    Create Valid Parentheses.cpp

---
+++ Valid Parentheses.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> stack;
+
+        for(int i=0;i<s.size();i++){
+            if(s[i] == '(' || s[i] == '{' || s[i] == '['){
+                stack.push(s[i]);
+            }
+            else {
+                if( stack.empty() ||(stack.top() == '(' && s[i] != ')'  ) || (stack.top() == '{' && s[i] != '}'  ) || (stack.top() == '[' && s[i] != ']'  )  ){
+                return false;
+                }
+                stack.pop();
+
+            }
+        }
+        return stack.empty();
+
+    }
+};
